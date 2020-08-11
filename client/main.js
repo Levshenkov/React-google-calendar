c16a8ec065d00f6a8fdceddd455588fdd299d51f
Levshenkov <levshenkov.denys@gmail.com>
2020-08-11 16:57:04 +0300

    eslint config added

--- client/main.js
+++ client/main.js
@@ -1,10 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 const Home = () => {
-    return <div></div>
-}
+    return <div>1</div>
+    }
 
-const target = document.getElementById('root')
+const target = document.getElementById('root');
 
-ReactDOM.render(<Home />, target)
+ReactDOM.render(<Home />, target);
--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,62 +1,70 @@
-const {resolve} = require('path')
-const CopyWebpackPlugin = require('copy-webpack-plugin')
-const MiniCSSExtractPlugin = require('mini-css-extract-plugin')
+/* eslint-disable import/no-extraneous-dependencies */
+const { resolve } = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
 
 const config = {
-    entry: './client/main.js',
-    mode: 'development',
-    output: {
-        filename: 'js/[name].bundle.js',
-        path: resolve(__dirname, 'dist'),
-        publicPath: '/'
+  entry: './client/main.js',
+  mode: 'development',
+  output: {
+    filename: 'js/[name].bundle.js',
+    path: resolve(__dirname, 'dist'),
+    publicPath: '/',
+  },
+  devServer: {
+    hot: true,
+    contentBase: resolve(__dirname, 'dist'),
+    port: 8080,
+    host: 'localhost',
+    index: 'index.html',
+    overlay: {
+      warnings: false,
+      errors: true,
     },
-    devServer: {
-        hot: true,
-        contentBase: resolve(__dirname, 'dist'),
-        port: 8080,
-        host: 'localhost',
-        index: 'index.html',
-        overlay: {
-            warnings: false,
-            errors: true
-        }
-    },
-    module: {
-        rules: [
-            {
-            test: /\.(js|jsx)$/,
-            loaders: ['babel_loader'],
-            exclude: /node_modules/
+  },
+  module: {
+    rules: [
+      {
+        enforce: 'pre',
+        test: /\.(js|jsx)$/,
+        loaders: ['eslint-loader'],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(js|jsx)$/,
+        loaders: ['babel_loader'],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.scss$/,
+        loaders: [
+          {
+            loader: MiniCSSExtractPlugin.loader,
+            options: {
+              publicPath: '../',
             },
-            {
-            test: /\.scss$/,
-            loaders: [
-                {loader: MiniCSSExtractPlugin.loader,
-                options: {
-                    publicPath: '../',
-                },
-            },
-                'css-loader',
-                'sass-loader'
-            ],
-            exclude: /node_modules/
-            },
+          },
+          'css-loader',
+          'sass-loader',
         ],
-    },
-    plugins: [
-        new MiniCSSExtractPlugin({
-            filename: 'css/main.css'
-        }),
-        new CopyWebpackPlugin({
-            patterns: [
-                {
-                    from : `${__dirname}/client/index.html`,
-                    to: 'index.html'
-                }
-            ]
-        })
-    ]
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new MiniCSSExtractPlugin({
+      filename: 'css/main.css',
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: `${__dirname}/client/index.html`,
+          to: 'index.html',
+        },
+      ],
+    }),
+  ],
 
-}
+};
 
-module.exports = config
+module.exports = config;
