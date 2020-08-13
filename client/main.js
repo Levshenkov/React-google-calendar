a5cbef9cc2850af3c12fc2b847a55a1a6dfc4633
Elvira Genkel <elvira.genkel@gmail.com>
2020-08-13 14:00:32 +0300

    added getting events from hardcoded test google calendar

--- client/main.js
+++ client/main.js
@@ -4,9 +4,11 @@ import Test from './components/test'
 import './main.css'
 
 const Home = () => {
+  const CALENDAR_ID = 'kcej035h7eidvmqc2cgcljchb0@group.calendar.google.com'
+  const API_KEY = 'AIzaSyB3PqDm4Ndi3j_N0DcaRk5lQzu7c4GnFKY'
   return (
     <div>
-      <Test />
+      <Test calendarId={CALENDAR_ID} apiKey={API_KEY} />
     </div>
   )
 }
